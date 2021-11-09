b34eabfebeec5fb04eddcefbc9040613850ae3a3
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-11-09 09:33:44 -0500

    Fixed dependencies

--- python/setup.py
+++ python/setup.py
@@ -12,10 +12,10 @@ setup(
     packages=find_packages(),
     install_requires=[
         "torch>=1.9.0", "torchsummary==1.5.1", "nnaudio==0.2.4", "timm==0.4.12",
-        "matplotlib==3.3.2", "catboost==0.24.2", "tensorboard==2.4.0",
-        "xgboost==1.2.0", "tqdm==4.50.2", "numpy==1.19.2", "seaborn==0.11.0",
-        "packaging==20.4", "pandas==1.1.3", "scikit_learn==0.23.2", "pyyaml",
-        "colorednoise==1.1.1", "librosa==0.8.1"
+        "matplotlib==3.3.2", "tensorboard==2.4.0", "tqdm==4.50.2",
+        "numpy==1.19.2", "seaborn==0.11.0", "packaging==20.4", "pandas==1.1.3",
+        "scikit_learn==0.23.2", "pyyaml", "colorednoise==1.1.1",
+        "librosa==0.8.1", "torchaudio>=0.9.0", "tsai"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
