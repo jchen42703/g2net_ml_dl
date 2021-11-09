90f1005a6c3c56f2775503a8fca933b98729b124
Takoda Denhof <tdenhof16@gmail.com>
2021-11-08 19:11:16 -0500

    Added Documentation

--- python/g2net/models/filter/rocketFilter.py
+++ python/g2net/models/filter/rocketFilter.py
@@ -8,7 +8,12 @@ class Filter():
 
 def load_train_csv(dir_to_csv):
     return pd.DataFrame(pd.read_csv(dir_to_csv))
-    
+
+""""Method to split training data
+*arg df, data frame from load_train_csv
+
+returns train, val , test dataframes
+"""
 def split_data(df):
     divideLength1 = round(len(df) * .9)
     divideLength2 = round(len(df)* .95)
@@ -16,7 +21,11 @@ def split_data(df):
     val = df[divideLength1:divideLength2]
     test = df[divideLength2:]
     return train,val ,test
+"""Method to train model 
+*arg train, training df from split_data
 
+returns classifier used to predict 
+"""
 def train(train):
     X_train = train['id']
     signals = []
@@ -31,11 +40,17 @@ def train(train):
     classifier.fit(X_training_transform, targets)
     return classifier
 
+"""Method to predict
+*arg classifier, classifier returned from training 
+*arg data, data to predict on 
+
+returns prediction of data
+"""
 def predict(classifier, data):
     ids = data['id']
     signals = []
-    for id in ids:
-        signal,target = G2NetDataset.__getitem__(id)
+    for i in ids:
+        signal,target = G2NetDataset.__getitem__(i)
         signals.append(signal)
     parameters = fit(signals)
     X_data = transform(signals,parameters)
