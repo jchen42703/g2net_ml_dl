a9315c24ccdf85e3c7deda2b7200310864568f4c
Takoda Denhof <tdenhof16@gmail.com>
2021-11-10 18:32:21 -0500

    Some Small Changes

--- python/g2net/models/filter/rocketFilter.py
+++ python/g2net/models/filter/rocketFilter.py
@@ -1,39 +1,18 @@
-from minirocket import fit, transform
+import numpy as np
 from sklearn.linear_model import RidgeClassifierCV
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sktime.transformations.panel.rocket import MiniRocket
 from g2net.io.dataset import G2NetDataset
 class Filter():
     def __init__():
         super().__init__()
-
-    def load_train_csv(dir_to_csv):
-        return pd.DataFrame(pd.read_csv(dir_to_csv))
-
-    """"Method to split training data
-    *arg df, data frame from load_train_csv
-
-    returns train, val , test dataframes
-    """
-    def split_data(df):
-        divideLength1 = round(len(df) * .9)
-        divideLength2 = round(len(df)* .95)
-        train = df[:divideLength1]
-        val = df[divideLength1:divideLength2]
-        test = df[divideLength2:]
-        return train,val ,test
+    
     """Method to train model 
     *arg train, training df from split_data
 
     returns classifier used to predict 
     """
-    def train(train):
-        X_train = train['id']
-        signals = []
-        targets = []
-        for i in X_train:
-            signal,target = G2NetDataset.__getitem__(i)
-            signals.append(signal)
-            targets.append(target)
+    def train(signals,targets):
         parameters = fit(signals)
         X_training_transform = transform(signals, parameters)
         classifier = RidgeClassifierCV(alphas = np.logspace(-3, 3, 10), normalize = True)
@@ -46,13 +25,10 @@ class Filter():
 
     returns prediction of data
     """
-    def predict(classifier, data):
-        ids = data['id']
-        signals = []
-        for i in ids:
-            signal,target = G2NetDataset.__getitem__(i)
-            signals.append(signal)
+    def predict(classifier, signals):
         parameters = fit(signals)
         X_data = transform(signals,parameters)
         return classifier.predict(X_data)
 
+    def score(classifier, signal_test, target_test):
+        return classifier.score(signal_test,target_test)
---
+++ python/tests/test_rocketFilter.py
@@ -0,0 +1,11 @@
+
+
+
+#%% Get Data & Imports
+from python.g2net.io.dataset import G2NetDataset
+import pandas as pd 
+paths = "./train_takoda.csv"
+dataset = G2NetDataset(paths)
+
+
+# %%
