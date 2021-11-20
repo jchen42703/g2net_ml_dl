e67a14eb9e6a04f498fa9200dc13e3ba5db0453c
Joseph Chen <jchen42703@gmail.com>
2021-11-20 01:55:31 -0500

    Added proper weights loading and integrated create_base_model

--- python/g2net/inference.py
+++ python/g2net/inference.py
@@ -8,7 +8,7 @@ from sklearn.metrics import roc_auc_score
 from g2net.io.dataset import G2NetDataset
 from g2net.io.transforms import Compose
 from g2net.models.filter import MiniRocket
-from g2net.models.base.architectures import SpectroCNN
+from g2net.models.base import create_base_model
 from g2net.train import create_base_transforms
 from g2net.utils.tsai import Timer
 
@@ -24,7 +24,6 @@ class Inferer(object):
     - The trial/fold
     - The performance on the test sets
     - Standard deviation and averages for all trials
-    - GPU memory...?
 
     Should be able to sample a distribution:
     - 1% are GW
@@ -33,17 +32,39 @@ class Inferer(object):
         - what is the predicted distributions for each type?
     """
 
-    def __init__(self, test_loader, threshold: float = 0.5):
+    def __init__(self,
+                 test_loader,
+                 base_model_paths: List[str],
+                 filter_model_paths: List[str],
+                 threshold: float = 0.5,
+                 num_folds: int = 5):
         """
         Args:
             test_loader: See create_test_dataloader
+            base_model_paths: paths to base models in order of folds
+            filter_model_paths: paths to filter models in order of folds
             threshold: the cutoff for sigmoid predicted outputs for 0 and 1.
+            num_folds: Should be = len(base_model_paths)
         """
         self.test_loader = test_loader
+        self.base_model_paths = base_model_paths
+        self.filter_model_paths = filter_model_paths
         self.timer = Timer()
         self.base_model = None
         self.filter_model = None
         self.threshold = threshold
+        self.num_folds = num_folds
+
+        if self.num_folds != len(self.base_model_paths):
+            raise ValueError(
+                "the number of folds should be the same as the number of base model weights"
+            )
+
+        if self.num_folds != len(self.filter_model_paths):
+            raise ValueError(
+                "the number of folds should be the same as the number of base model weights"
+            )
+
         cols = [
             "fold", "base_auc", "base_time", "mr_auc", "mr_time", "both_auc",
             "both_time"
@@ -74,18 +95,24 @@ class Inferer(object):
             f'{metric_key}_time': time_elapsed,
         }
 
-    def infer_minirocket_only(self) -> dict:
+    def infer_minirocket_only(self, fold) -> dict:
         """Infers minirocket only. Returns a dictionary of metrics
         """
         # loads model and weights
         self.filter_model = MiniRocket()
+        load_weights(self.filter_model, self.filter_model_paths[fold])
+        self.filter_model.eval()
+
         return self.infer_single(self.filter_model, "mr")
 
-    def infer_base_only(self) -> dict:
+    def infer_base_only(self, fold) -> dict:
         """Infers with only the base model. Returns a dictionary of metrics
         """
         # loads model and weights
-        self.base_model = SpectroCNN()
+        self.base_model = create_base_model()
+        load_weights(self.base_model, self.base_model_paths[fold])
+        self.base_model.eval()
+
         # Do inference
         return self.infer_single(self.base_model, "base")
 
@@ -120,13 +147,14 @@ class Inferer(object):
             f'both_time': time_elapsed,
         }
 
-    def infer_single_fold(self):
+    def infer_single_fold(self, fold: int):
         """Explicit inference with minirocket as the filter model and the
         baseline solution.
         """
+        # TO DO: Inference with class sampling
         # Load models and weights
-        mr_only_metrics = self.infer_minirocket_only()
-        base_only_metrics = self.infer_base_only()
+        mr_only_metrics = self.infer_minirocket_only(fold)
+        base_only_metrics = self.infer_base_only(fold)
         both_metrics = self.infer_all_both(self.filter_model, self.base_model)
         fold_metrics = {**mr_only_metrics, **base_only_metrics, **both_metrics}
         return fold_metrics
@@ -134,9 +162,10 @@ class Inferer(object):
     def infer_all(self):
         """Inference on all folds
         """
-        for fold in num_folds:
-            metrics = self.infer_single_fold()
+        for fold in self.num_folds:
+            metrics = self.infer_single_fold(fold)
             self.metrics.append({"fold": fold, **metrics})
+        # TO DO: compute averages and std dev and append the row
 
 
 def infer_auc(y_true: torch.Tensor, y_pred: torch.Tensor) -> float:
@@ -147,8 +176,14 @@ def infer_auc(y_true: torch.Tensor, y_pred: torch.Tensor) -> float:
     return roc_auc_score(y, pred)
 
 
-def load_weights(model, weights):
-    pass
+def load_weights(model: torch.nn.Module, weights_path: str):
+    """Loads weights into model from weights_path.
+
+    The state dict from the baseline has the keys:
+        "global_epoch", "model", "state", "all_states"
+    To load the model weights, only use state_dict["model"].
+    """
+    model.load_state_dict(torch.load(weights_path)["model"])
 
 
 def create_test_transforms(maxes: List[float] = [4.61e-20, 4.23e-20, 1.11e-20],
