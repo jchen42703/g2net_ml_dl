e587876a4f7527fccb879f9ea5a9c8ddc10e36a6
Joseph Chen <jchen42703@gmail.com>
2021-11-21 00:12:30 -0500

    Removed assert statement for cuda

--- python/g2net/inference.py
+++ python/g2net/inference.py
@@ -79,9 +79,8 @@ class Inferrer(object):
         # Single GPU inference
         for batch in self.test_loader:
             if not self.cpu_only:
-                assert "cuda" in str(batch[0].device), \
-                    "device should be cuda but is: " + str(batch[0].device)
                 batch[0] = batch[0].cuda()
+
             pred = predict_binary(model, batch[0])
             # thresholding
             pred[pred >= self.threshold] = 1
