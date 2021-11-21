3284bbb1e836fa7c8983e757a3642ae7fd7bf163
Joseph Chen <jchen42703@gmail.com>
2021-11-21 03:52:13 -0500

    Integrated GlobalEvaluator

--- python/scripts/infer.py
+++ python/scripts/infer.py
@@ -3,7 +3,8 @@ if __name__ == "__main__":
     import os
     import pandas as pd
     from g2net.utils.config_reader import load_config
-    from g2net.inference import Inferrer, create_test_transforms, create_test_dataloader
+    from g2net.inference import GlobalEvaluator, Inferrer, create_test_transforms, create_test_dataloader
+    from glob import glob
     import pprint
 
     parser = argparse.ArgumentParser(description="For training.")
@@ -27,12 +28,28 @@ if __name__ == "__main__":
     test_df = pd.read_csv(test_path).iloc[start:end]
 
     transforms = create_test_transforms()
-    test_loader = create_test_dataloader(test_df, batch_size=cfg["batch_size"])
+    test_loader = create_test_dataloader(test_df,
+                                         batch_size=cfg["batch_size"],
+                                         test_transforms=transforms)
     inferrer = Inferrer(test_loader,
                         cfg["base_model_paths"],
                         cfg["filter_model_paths"],
+                        cfg["export_dir"],
                         threshold=cfg["threshold"],
                         cpu_only=cfg["cpu_only"])
     inferrer.infer_all()
-    csv_path = os.path.join(cfg["export_dir"], "inference_metrics.csv")
+    csv_path = os.path.join(cfg["export_dir"], "inference_times.csv")
     inferrer.metrics.to_csv(csv_path)
+
+    base_model_paths = glob(
+        os.path.join(cfg["export_dir"], "base_preds", "*.npy"))
+    filter_preds_paths = glob(
+        os.path.join(cfg["export_dir"], "filter_preds", "*.npy"))
+    both_preds_paths = glob(
+        os.path.join(cfg["export_dir"], "both_preds", "*.npy"))
+
+    evaluator = GlobalEvaluator(test_loader, base_model_paths,
+                                filter_preds_paths, both_preds_paths)
+    evaluator.evaluate_all()
+    csv_path = os.path.join(cfg["export_dir"], "inference_metrics.csv")
+    evaluator.metrics.to_csv(csv_path)
